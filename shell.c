32a240585a667084facd279d5b937ebfac0e0594
JayantRautela <jayantrautela0205@gmail.com>
2026-09-09 01:29:49 +0530

    refactor(shell): bind the shell in loop

--- shell.c
+++ shell.c
@@ -5,42 +5,47 @@
 
 int main(int argc, char *argv[])
 {
-  // array to store the input buffer 
-	char buffer[1024];
-
-	printf("$ ");
-
-	fgets(buffer, 1024, stdin);
-
-  char *nl = strchr(buffer, '\n');
-
-  if (nl) *nl = '\0';
-
-  // splliting the command
-  char *args[20];
-  int cnt = 0;
-
-  args[cnt] = strtok(buffer, " ");
-
-  while (args[cnt] != NULL) 
+  while (1)
   {
-    args[++cnt] = strtok(NULL, " ");
-  }
+    // array to store the input buffer 
+	  char buffer[1024];
 
-	// running the command
-  pid_t pid = fork();
+	  printf("$ ");
 
-  if (pid > 0)
-  {
-    // parent
-    wait(NULL);
-  }
-  else
-  {
-    //child
-    execvp(args[0], args);
+	  fgets(buffer, 1024, stdin);
 
-    // exec error
-    fprintf(stderr, "Could not exec %s\n", buffer);
+    char *nl = strchr(buffer, '\n');
+
+    if (nl) *nl = '\0';
+
+    // splliting the command
+    char *args[20];
+    int cnt = 0;
+
+    args[cnt] = strtok(buffer, " ");
+
+    while (args[cnt] != NULL) 
+    {
+      args[++cnt] = strtok(NULL, " ");
+    }
+
+    if (strcmp(args[0], "exit") == 0) exit(0);
+
+	  // running the command
+    pid_t pid = fork();
+
+    if (pid > 0)
+    {
+      // parent
+      wait(NULL);
+    }
+    else
+    {
+      //child
+      execvp(args[0], args);
+
+      // exec error
+      fprintf(stderr, "Could not exec %s\n", buffer);
+    }
   }
 }
